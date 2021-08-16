Fix Fisher Calculation, Model now appends Logits and Softmax Probability (will fix the training py code at a later date). Cleaned up unused Fisher Code

networks/e2attention.py
@@ -141,15 +141,16 @@ class DNSteerableAGRadGalNet(nn.Module):
                     self.classifier = nn.Linear(concat_length, n_classes)
                     self.aggregate = self.aggregation_ds
                 elif aggregation_mode == 'ft':
-                    self.classifier = nn.Linear(n_classes*self.ag, n_classes)
+                    self.classifier = nn.Linear(n_classes*self.ag, n_classes) #times4
                     self.aggregate = self.aggregation_ft
                 else:
                     raise NotImplementedError
         else:
-            self.classifier = nn.Linear((150//16)**2*64, n_classes)
+            self.classifier = nn.Linear(((150//16)**2*64), n_classes)
             self.aggregate = lambda x: self.classifier(self.flatten(x))
 
-
+        #Set up random weights for the last classifier layer
+        
         ####################
         # I wonder if I can get away with this? I dont know.
         # initialise weights
@@ -295,7 +296,7 @@ class DNSteerableAGRadGalNet(nn.Module):
                 raise NotImplementedError
             return out
         else:
-            return F.softmax(out,dim=1)
+            return out, F.softmax(out,dim=1)
 
 
 

utils/utils.py
@@ -64,7 +64,14 @@ def load_model(config, load_model='best', path_supliment='', device=torch.device
         MODEL_PATH = config['output']['directory'] +'/'+ path_supliment + str(load_model) + '.pt'
     
     model = globals()[config['model']['base']](**config['model']).to(device)
-    model.load_state_dict(torch.load(MODEL_PATH))
+    pretrained_dict = torch.load(MODEL_PATH)
+    model_dict = model.state_dict()
+    #modified the load_state_dict to allow for skipping layers not in the pt file
+    pretrained_dict = {k: v for k, v in pretrained_dict.items() if (k in model_dict) and (model_dict[k].shape == pretrained_dict[k].shape)}
+    # 2. overwrite entries in the existing state dict
+    model_dict.update(pretrained_dict)
+    model.load_state_dict(model_dict, strict=False)
+    
     model.eval()
     
     return model