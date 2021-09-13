92b562015298a1af1013d686db221a0d8e6f2308
milpan <matthew.bromley@gmail.com>
2021-09-13 15:29:10 +0100

    Implemented Effective Dimension and new Fisher Calculation, now able to specify for the emperical fisher aswell

--- utils/fisher.py
+++ utils/fisher.py
@@ -14,6 +14,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import configparser as ConfigParser
 from tqdm.notebook import trange, tqdm
+from scipy.special import logsumexp
+
 import utils
 # Ipmport various network architectures
 from networks import AGRadGalNet, DNSteerableLeNet, DNSteerableAGRadGalNet #e2cnn module only works in python3.7+
@@ -86,67 +88,79 @@ def jacobian(f, w, create_graph=False):
     output has shape (len(f), len(x))
     """
 
-    jac = []                                                                                          
+    jac = []
+    output = f
+    #f = torch.log(f)
     grad_f = torch.zeros_like(f)
-
+    
     for i in range(len(f)):                                                                      
         grad_f[i] = 1.
         grad_f_x = torch.autograd.grad(f, w, grad_f, retain_graph=True, create_graph=create_graph, allow_unused=True)
         J = torch.cat(grad_f_x).view(-1)
-        jac.append(J)
+        jac.append(J * torch.sqrt(output[i]))
         grad_f[i] = 0.
                                                   
     return torch.stack(jac).reshape(f.shape + J.shape)
-
 #---MAIN FISHER INITALISATION---#
 '''
 net --> PyTorch Compatiable Model
 train_loader --> PyTorch/SKLearn Compatiable DataLoader Function
 n_iterations --> Number of realisations of the fisher matrix
 '''
-def CalcFIM(net, train_loader, n_iterations):
+def CalcFIM(net, train_loader, n_iterations, approximation=None):
     #First we need to obtain the number of weights in the last fully connected layer and freeze the weights in every other layer
     Rank = []
     FR = []
     print(f"Calculating {n_iterations} of the Fisher...")
     Number_of_FisherIts = n_iterations;
     n_weight = 12
-    realisations_torch = torch.zeros((Number_of_FisherIts,n_weight,n_weight)).to(device) #All fishers
+    
+    realisations_torch = torch.zeros((Number_of_FisherIts,n_weight,n_weight)) #All fishers
     for i in tqdm(range(Number_of_FisherIts)):
-        #torch.nn.init.uniform_(net.classifier.weight, -1., 1.)
+        torch.nn.init.uniform_(net.classifier.weight, -1., 1.)
+        #torch.nn.init.normal_(net.classifier.weight, mean=0.0, std=1.0)
         #torch.nn.init.xavier_uniform_(net.classifier.weight, gain=1.0)
         #torch.nn.init.kaiming_normal(net.classifier.weight)
-        torch.nn.init.orthogonal_(net.classifier.weight, gain=1.0)
+        #torch.nn.init.orthogonal_(net.classifier.weight, gain=1.0)
         w = net.classifier.weight
         flat_w = w.view(-1).cpu()
-        try:
-            fisher = torch.zeros((n_weight,n_weight)).to('cuda')
-            for x_n, y_n in train_loader:
-                x_n, y_n = x_n.to('cuda'), y_n.to('cuda')
-                f_n = net(x_n)[0]
-                for row in f_n:
+        fisher = torch.zeros((n_weight,n_weight)).to('cuda')
+        for x_n, y_n in train_loader:
+            x_n, y_n = x_n.to('cuda'), y_n.to('cuda')
+            f_n = net(x_n)[1]
+            summedfisher = np.zeros((Number_of_FisherIts, 12, 12))
+            for row in f_n:
+                if (approximation=="emperical"):
                     pi_n = F.softmax(row, dim=0)
                     diag_pi_n = torch.diag(pi_n.squeeze(0)).to('cuda')
-                    pi_n_pi_n_T= torch.from_numpy(np.outer(pi_n.cpu().detach().numpy(),pi_n.cpu().detach().numpy())).to('cuda')
-                    J_f = jacobian((torch.squeeze(row,0)),w)
+                    pi_n_pi_n_T=torch.from_numpy(np.outer(pi_n.cpu().detach().numpy(),np.transpose(pi_n.cpu().detach().numpy()))).to('cuda')
+                #J_f = get_gradient(row, w)
+                J_f = jacobian((torch.squeeze(row,0)),w)
+                jacob1 = J_f.cpu()
+                if (approximation=="emperical"):   
                     J_f_T = J_f.permute(1,0)
                     K2 = diag_pi_n-pi_n_pi_n_T
                     K3 = K2.cuda()
                     fisher += torch.matmul((torch.matmul(J_f_T,K3)),J_f)
-        except:
-            print("\n A fisher failed to be realised, skipping...")
-            
-        try:        
+                else:
+                    temp_sum = np.zeros((2, 12, 12))
+                    grads = jacob1.detach().numpy()
+                    for j in range(2):
+                        temp_sum[j] += np.array(np.outer(grads[j], np.transpose(grads[j])))
+                    summedfisher[i] += np.sum(temp_sum, axis=0)
+                    fisher += torch.from_numpy(summedfisher[i]).cuda()
             with torch.no_grad():
-                rank = torch.matrix_rank(fisher).item()
-                Rank.append(rank)
-                realisations_torch[i] = fisher.cpu()
-                Fw = np.matmul(fisher.cpu().numpy(),flat_w)
-                wFw = np.dot(flat_w,Fw)
-                FR.append(wFw)
-        except:
-            print("Error occured when trying to calculate Rank, skipping...")
+                try:
+                    rank = torch.matrix_rank(fisher).item()
+                    Rank.append(rank)
+                    realisations_torch[i] = fisher.cpu()
+                    Fw = np.matmul(fisher.cpu().numpy(),flat_w)
+                    wFw = np.dot(flat_w,Fw)
+                    FR.append(wFw)
+                except:
+                    print("Discarding a Rank Calculation")
     return realisations_torch, Rank, FR;
+
 
 def Hessian(y, x):
     hessian = Jacobian(Jacobian(y, x, create_graph=False), x)
@@ -159,6 +173,26 @@ def calc_eig(Fishers):
     return eigval
     
 def normalise(Fishers):
-    num_samples = len(Fishers)
-    TrF_integral = (1 / num_samples) * np.sum(np.array([torch.trace(F) for F in Fishers]))
-    return [((12) / TrF_integral) * F for F in Fishers]
+    fisher_trace = np.trace(np.average(Fishers, axis=0)) 
+    finalfisher = np.average(Fishers, axis=0)
+    fhat = 12 * finalfisher/fisher_trace
+    return fhat
+
+#Functions to calculate the Effective Dimension as computed in https://zenodo.org/record/4732830 by Amira Abbas
+def effective_dimension(model, f_hat, num_thetas, n, outputs):
+    '''
+    model -> pytorch compatible model
+    f_hat -> normalised array of fisher matrix
+    n -> Number of samples for the computation (len trainloader?) in list
+    outputs -> Number of classes
+    '''
+    effective_dim = []
+
+    for ns in tqdm(n):
+        Fhat = f_hat * ns / (2 * np.pi * np.log(ns))
+        one_plus_F = np.eye(outputs) + Fhat
+        det = np.linalg.slogdet(one_plus_F)[1]  # log det because of overflow
+        r = det / 2  # divide by 2 because of sqrt
+        effective_dim.append(2 * (logsumexp(r) - np.log(num_thetas)) / np.log(ns / (2 * np.pi * np.log(ns))))
+
+    return effective_dim
